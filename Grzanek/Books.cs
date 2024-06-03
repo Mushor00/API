d81222516fe405873cec5afeceb8d86266975372
Mushor00 <114734@student.san.edu.pl>
2024-06-03 16:30:59 +0200

    update

--- Grzanek/Books.cs
+++ Grzanek/Books.cs
@@ -1,11 +1,15 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace API
 {
     public class Books
     {
+        [Required]
         public DateOnly Data { get; set; }
-
+        [Required]
         public string? Tytul { get; set; }
-
+        [Required]
         public string? Autor { get; set; }
 
     }
--- Grzanek/ToRead.cs
+++ Grzanek/ToRead.cs
@@ -1,9 +1,12 @@
-﻿namespace API
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API
 {
     public class ToRead
     {
+        [Required]
         public string Tytul { get; set; }
-
+        [Required]
         public string Autor { get; set; }
 
     }
