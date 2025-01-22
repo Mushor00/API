5a0ddf234dca2c1818af3b4bbc7716e1a6c74a0a
KwiaTek02 <matik775@gmail.com>
2025-01-22 21:48:12 +0100

    22.01.2025 21:47 MK

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+//builder.Services.AddScoped<BooksService>();
 
 var app = builder.Build();
 
---
+++ Grzanek/Services/BooksService.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+using System.Threading.Tasks;
+using API;
+
+public class BooksService
+{
+    private readonly HttpClient _httpClient;
+
+    public BooksService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Books>> GetBooks()
+    {
+        return await _httpClient.GetFromJsonAsync<List<Books>>("/Books/allbooks");
+    }
+
+    public async Task<List<ToRead>> GetToReadBooks()
+    {
+        return await _httpClient.GetFromJsonAsync<List<ToRead>>("/ToRead");
+    }
+
+    public async Task AddBook(Books book)
+    {
+        await _httpClient.PostAsJsonAsync("/Books/addbooks", book);
+    }
+
+    public async Task AddToReadBook(ToRead book)
+    {
+        await _httpClient.PostAsJsonAsync("/ToRead", book);
+    }
+
+    public async Task DeleteBook(Books book)
+    {
+        await _httpClient.DeleteAsync($"/Books/deletebooks?Tytul={book.Tytul}");
+    }
+}
